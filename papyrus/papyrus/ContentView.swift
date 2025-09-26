79ae57734389fa469cae6ba0766a9b61817f2346
Isaac Akalanne <isaacakalanne@gmail.com>
2025-09-26 19:16:33 +0100

    Initial commit

---
+++ papyrus/papyrus/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  papyrus
+//
+//  Created by Isaac Akalanne on 26/09/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ papyrus/papyrusTests/papyrusTests.swift
@@ -0,0 +1,16 @@
+//
+//  papyrusTests.swift
+//  papyrusTests
+//
+//  Created by Isaac Akalanne on 26/09/2025.
+//
+
+import Testing
+
+struct papyrusTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
