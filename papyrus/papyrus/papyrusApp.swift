ed87712a0bdd3b91a8ffc60133ff99e87d769708
Isaac Akalanne <>
2025-09-26 19:16:33 +0100

    Initial commit

---
+++ papyrus/papyrus/papyrusApp.swift
@@ -0,0 +1,17 @@
+//
+//  papyrusApp.swift
+//  papyrus
+//
+//  Created by Isaac Akalanne on 26/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct papyrusApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
