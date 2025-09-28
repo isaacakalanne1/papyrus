a74a8b59b8aed2005fd9fa5d74588a77d8b1ff90
Isaac Akalanne <>
2025-09-28 12:32:48 +0100

    Added load and save stories functionality

--- papyrus/papyrus/Packages/Reader/Sources/Reader/Redux/ReaderState.swift
+++ papyrus/papyrus/Packages/Reader/Sources/Reader/Redux/ReaderState.swift
@@ -10,18 +10,21 @@ import TextGeneration
 public struct ReaderState: Equatable {
     var mainCharacter: String
     var setting: String
+    var loadedStories: [Story]?
     var story: Story?
     var isLoading: Bool
 
     public init(
         mainCharacter: String = "",
         setting: String = "",
+        loadedStories: [Story]? = nil,
         story: Story? = nil,
         isLoading: Bool = false
     ) {
-        self.story = story
         self.mainCharacter = mainCharacter
         self.setting = setting
+        self.loadedStories = loadedStories
+        self.story = story
         self.isLoading = isLoading
     }
 }
--- papyrus/papyrus/Packages/Reader/Sources/Reader/Services/ReaderEnvironment.swift
+++ papyrus/papyrus/Packages/Reader/Sources/Reader/Services/ReaderEnvironment.swift
@@ -5,11 +5,15 @@
 //  Created by Isaac Akalanne on 27/09/2025.
 //
 
+import Foundation
 import TextGeneration
 
 public protocol ReaderEnvironmentProtocol {
     func createChapter(story: Story) async throws -> Story
     func saveStory(_ story: Story) async throws
+    func loadStory(withId id: UUID) async throws -> Story?
+    func getAllSavedStoryIds() async throws -> [UUID]
+    func deleteStory(withId id: UUID) async throws
 }
 
 public struct ReaderEnvironment: ReaderEnvironmentProtocol {
@@ -28,6 +32,18 @@ public struct ReaderEnvironment: ReaderEnvironmentProtocol {
     }
     
     public func saveStory(_ story: Story) async throws {
-        
+        try await dataStore.saveStory(story)
+    }
+    
+    public func loadStory(withId id: UUID) async throws -> Story? {
+        try await dataStore.loadStory(withId: id)
+    }
+    
+    public func getAllSavedStoryIds() async throws -> [UUID] {
+        try await dataStore.getAllSavedStoryIds()
+    }
+    
+    public func deleteStory(withId id: UUID) async throws {
+        try await dataStore.deleteStory(withId: id)
     }
 }
