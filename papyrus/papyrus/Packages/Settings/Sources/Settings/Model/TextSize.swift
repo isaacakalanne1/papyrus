3222888ba137faa731651e372ebdbe46e7c2b1b2
Isaac Akalanne <isaacakalanne@gmail.com>
2025-10-03 15:14:37 +0100

    Updated TextSize option appearance

--- papyrus/papyrus/Packages/Settings/Sources/Settings/Model/TextSize.swift
+++ papyrus/papyrus/Packages/Settings/Sources/Settings/Model/TextSize.swift
@@ -6,19 +6,6 @@ public enum TextSize: String, CaseIterable, Codable, Sendable {
     case large
     case extraLarge
     
-    public var displayName: String {
-        switch self {
-        case .small:
-            return "Small"
-        case .medium:
-            return "Medium"
-        case .large:
-            return "Large"
-        case .extraLarge:
-            return "Extra Large"
-        }
-    }
-    
     public var fontSize: CGFloat {
         switch self {
         case .small:
