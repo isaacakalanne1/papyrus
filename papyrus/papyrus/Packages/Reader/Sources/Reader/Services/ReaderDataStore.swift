d5dd8de234b148f5df886a7120c2bfa4d3423cdc
Isaac Akalanne <isaacakalanne@gmail.com>
2026-01-05 21:03:30 +0000

    Revert "Added new files (+10 squashed commits)"
    This reverts commit 4f7f01d1ad78450530301e1381a8979d611b05ee. (+3 squashed commits)
    Squashed commits:
    [3d99a93] Empty commit
    [4f7f01d] Added new files (+10 squashed commits)
    Squashed commits:
    [193efab] Revert "Updated to use @available tags (+2 squashed commits)"
    
    This reverts commit d4c5ab4aae208b9654afe8783a09c77f5931f78a.
    [d4c5ab4] Updated to use @available tags (+2 squashed commits)
    Squashed commits:
    [d029e51] Updated versions
    [524a0ba] Network operation performer (alba)
    [e59d2f2] Reader redux updates
    [d750873] Updated tests
    [02ac6c0] Added auto-create chapter
    [6ddb76e] Revert "More updates (+3 squashed commits)"
    
    This reverts commit 298895758800a12c1b5e7e85ea595e8a29daac10. (+6 squashed commits)
    Squashed commits:
    [0fbf1d0] More updates
    [9025654] Updated decoder
    [9b18a57] Updated subscription logic to check on load
    [925acc9] Added auto-create chapter functionality
    [2988957] More updates (+3 squashed commits)
    Squashed commits:
    [9025654] Updated decoder
    [9b18a57] Updated subscription logic to check on load
    [925acc9] Added auto-create chapter functionality
    [f33da72] Added optional try in case of individual corrupted stories
    
    Updated subscription logic to check on load
    
    Updated decoder
    
    More updates
    
    Revert "More updates (+3 squashed commits)"
    
    This reverts commit 298895758800a12c1b5e7e85ea595e8a29daac10.
    [a85bee5] Fixed scroll offset logic
    [ffeedc6] Removed duplicate cases
    [603408b] Simplified scroll timer logic
    [bf40958] Simplified endpoints
    Updated models used for theme, plot outline, etc
    [1deb499] Added loaded subscriptions logic

--- papyrus/papyrus/Packages/Reader/Sources/Reader/Services/ReaderDataStore.swift
+++ papyrus/papyrus/Packages/Reader/Sources/Reader/Services/ReaderDataStore.swift
@@ -16,6 +16,7 @@ public protocol ReaderDataStoreProtocol {
 }
 
 public class ReaderDataStore: ReaderDataStoreProtocol {
+
     private let fileManager = FileManager.default
     private let encoder = JSONEncoder()
     private let decoder = JSONDecoder()
